b2acb66256f5b90f73f06e9fdbd6d2ebebac3dc8
d0745853 <56074830+d0745853@users.noreply.github.com>
2019-12-27 01:53:19 +0800

    加註解

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,3 +1,4 @@
+//宣告變數
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -6,16 +7,18 @@ const io = require('socket.io')(server);
 // 加入線上人數計數
 let onlineCount = 0;
 
+//推到瀏覽器上顯示
 app.get('/', (req, res) => {
-    res.sendFile( __dirname + '/chatroom.html');
+    res.sendFile( __dirname + '/view/chatroom.html');
 });
-
+// 當觸發連線事件
 io.on('connection', (socket) => {
     // 有連線發生時增加人數
     onlineCount++;
     // 發送人數給網頁
     io.emit("online", onlineCount);
-
+    // 接收來自前端的 greet 事件
+    // 然後回送 greet 事件，並附帶內容
     socket.on("greet", () => {
         socket.emit("greet", onlineCount);
     });
@@ -35,7 +38,7 @@ io.on('connection', (socket) => {
         io.emit("online", onlineCount);
     });
 });
-
+// 伺服器位置
 server.listen(3000, () => {
     console.log("Server Started. http://localhost:3000");
 });
